417c41d71c9395fff837a6db2b9bc549f625707b
kkaur378 <300988052@cencol.ca>
2018-06-20 11:16:22 -0400

    Add to java script

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,10 +1,18 @@
-// setup your IIFE (Immediately Invoked Function Expression)
-(function () {
-"use strict";
+(function () 
+{
 
+    "use strict";
 
-// YOUR CODE GOES HERE
+    let SubmitButton = document.getElementById("SendButton");
 
-
+            SubmitButton.addEventListener("click", (event) =>
+             {
+    
+                if (!document.forms[0].checkValidity())
+                {
+                    OutputFormDataToConsole();
+                    ValidateForm();
+                }
+            })
+    
 })();
-
